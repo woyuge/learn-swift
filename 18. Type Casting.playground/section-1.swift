c44c25cc81ef657dae2f11dbe0eb8c97ac103269
20n9ha0.L1 <tsunghao.lee@gmail.com>
2015-03-19 00:45:59 +0800

    18. Type Casting.playground

--- 18. Type Casting.playground/section-1.swift
+++ 18. Type Casting.playground/section-1.swift
@@ -1,15 +1,14 @@
 // ------------------------------------------------------------------------------------------------
-// Things to know:
+// 本篇須知：
 //
-// * Type casting allows us to check the type of an instance and/or treat that instance as a
-//   different type from somewhere else in its own hieararchy.
+// * 型別轉換(type casting)允許我們檢查一個實體的型別後，在這個實體的階層中把它當作另一個不同的型別來操作
 //
-// * Type casting also allows us to determine if a type conforms to a protocol.
+// * 型別轉換也允許我們確定一個型別是否符合某一個協定
 //
-// * Additionally we can create
+// * 其餘的部份看範例程式碼來解釋
 // ------------------------------------------------------------------------------------------------
 
-// Let's start by creating a few types to work with:
+// 讓我們創建一些型別來做處理：
 class MediaItem
 {
 	var name: String
@@ -36,8 +35,7 @@ class Song: MediaItem
 	}
 }
 
-// We'll create a library of Movies and Songs. Note that Swift will infer the type of the array to
-// be MediaItem[].
+// 我們將創建一個擁有 Movie 以及 Song 的 library，注意，Swift 將自動推斷 library 的型別為 MediaItem[]
 let library =
 [
 	Movie(name: "Casablanca", director: "Michael Curtiz"),
@@ -48,14 +46,13 @@ let library =
 ]
 
 // ------------------------------------------------------------------------------------------------
-// Checking type
+// 檢查型別
 //
-// We can check the type of an object very simply with the 'is' operator, also known as the Type
-// Check Operator.
+// 我們可以使用被稱為型別檢查運算子的 'is' 運算子來輕易地檢查一個物件的型別
 library[0] is Movie // true
 library[0] is Song  // false
 
-// Let's see this in action. Lets loop through the library and count up the movies and songs:
+// 讓我們看看實際運用的結果。讓我們遍歷這個 library 陣列並且計數其中包含的 movies 以及 songs：
 var movieCount = 0
 var songCount = 0
 for item in library
@@ -64,24 +61,22 @@ for item in library
 	else if item is Song { ++songCount }
 }
 
-// Our final Movie and Song counts:
+// 我們最終包含的 movies 以及 songs 的數目為：
 movieCount
 songCount
 
 // ------------------------------------------------------------------------------------------------
-// Downcasting
+// 向下轉型
 //
-// As with other languages, downcasting refers to casting an object to a subclass within its
-// hierarchy. This is done with the Type Cast Operator, "as".
+// 跟其他語言一樣，向下轉型指在一個物件的階層中，將它的型別轉換為它子類別的型別。這個動作可以使用型別轉換運算子 "as"
+// 來達成
 //
-// Because not all objects can be downcasted, the result of a downcast may be nil. Because of this,
-// we have two options for downcasting. We can downcast to an optional using the "as?" operator
-// or downcast with forced unwrapping of the optional with the standard "as" operator. As with
-// forced unwrapping, only do this if you know for certain that the downcast will succeed.
+// 因為不是所有的物件都能被向下轉型，所以轉換的結果可能是 nil。正因如此，我們在轉型過程中需要兩個可選型別。我們可以使
+// 用 "as?" 運算子向下轉換到一個可選型別，或者是使用 "as" 運算子向下轉型到一個強制解開的可選型別。以強制解開的可選型
+// 別而言，除非你確定向下轉型一定成功，否則別直接這麼做
 //
-// Let's see this in action using optional binding and the "as?" operator. Remember that our
-// library has type MediaItem[], so if we cast an element of that array to either a Movie or Song,
-// will will be downcasting the instance.
+// 讓我們看看使用可選綁定以及 "as?" 運算子實際運作的狀況。記得，我們的 library 型別是 MediaItem[]，所以如果我們將
+// 一個陣列中的一個元素轉型為 Movie 或是 Song，就表示對實體做向下轉型
 for item in library
 {
 	if let movie = item as? Movie
@@ -95,18 +90,18 @@ for item in library
 }
 
 // ------------------------------------------------------------------------------------------------
-// Type Casting for Any and AnyObject
+// 對 Any 以及 AnyObject 做向下轉型
 //
-// * AnyObject allows us to store an instance to any class type.
 //
-// * Any allows us to store a reference to any type at all, excluding functino types.
+// * AnyObject 允許我們使用任意的類別型別來儲存一個實體
 //
-// We should strive to use Any and AnyObject only when we must (like when an API function returns
-// arrays of values of any object type.) Otherwise, it's important that we use explicit types to
-// conform to Swift's type safety.
+// * Any 允許我們儲存除了函式型別的任意型別
 //
-// Let's see AnyObject in action. We'll define an array of type AnyObject[] and populate it with
-// some movies:
+// 我們應當儘可能地在必要的時候才使用 Any 以及 AnyObject(例如當 API 函式回傳了一個包含了 AnyObject 元素的陣列)
+// 否則，為了在 Swift 中達到型別安全，應該顯式地指定物件的型別
+//
+// 讓我們看看 AnyObject 實際運作的狀況。我們將定義一個型別為 AnyObject[] 的陣列，然後把一些 Movie 實體放進這個
+// 陣列中：
 let someObjects: [AnyObject] =
 [
 	Movie(name: "2001: A Space Odyssey", director: "Stanley Kubrick"),
@@ -114,37 +109,33 @@ let someObjects: [AnyObject] =
 	Movie(name: "Alien", director: "Ridley Scott"),
 ]
 
-// Here, we know that someObjects[] only contains Movie instances, so we'll use our forced version
-// of the typecast operator, "as". Note, however, that if somebody modifies the code later and adds
-// an instance of a non-Movie type (which they can do), we'll crash. This is why it's important
-// to limit our use of AnyObject and Any to only those cases where we absolutely need it.
+// 這裡，我們曉得 someObjects[] 只儲存了 Movie 實體，所以我們使用 "as" 型別轉換運算子來對它強制轉型。然而，如果之
+// 後某個人在這個陣列中加入了不是 Movie 型別的實體(他們確實做得到)，程序將崩潰。這就是為什麼我們應該在只在有必要的時
+// 候才去使用 AnyObject 以及 Any 的原因
 //
-// Let's see how we would use the someObjects array:
+// 讓我們看看該如何使用這個 someObjects[]：
 for object: AnyObject in someObjects
 {
-	let movie = object as Movie
+	let movie = object as! Movie
 	"Movie: '\(movie.name)' was directed by \(movie.director)"
 }
 
-// Alternatively, we can downcast the array itself rather than each item:
-var someMovies = someObjects as [Movie]
+// 另外，我們可以將整個陣列向下轉型，而不是一個個元素分別處理：
+var someMovies = someObjects as! [Movie]
 for movie in someMovies
 {
 	"Movie: '\(movie.name)' was directed by \(movie.director)"
 }
 
-// Finally, we can avoid the additional local variable and performt he downcast right inside
-// the loop structure:
-for movie in someObjects as [Movie]
+// 最後，我們可以避免使用多餘的區域變數，並且在循環結構中直接使用向下轉型：
+for movie in someObjects as! [Movie]
 {
 	"Movie: '\(movie.name)' was directed by \(movie.director)"
 }
 
-// Any allows us store references to any type at all (not including functions), which can include
-// integers, floating points, strings or objects.
+// Any 允許我們儲存除了函式型別外的任意型別，這包含了整數、浮點數、字串或是物件
 //
-// Let's see this in action. We'll create an array of type Any[] and fill it with random bits and
-// pieces of stuff:
+// 讓我們看看實際運作情況如何。我們將創建一個任意型別的陣列 Any[]，並且隨意放各種不同的東西在裡頭：
 var things = [Any]()
 
 things.append(0)
@@ -155,13 +146,11 @@ things.append("Hello")
 things.append((3.0, 5.0))
 things.append(Movie(name: "Ghostbusters", director: "Ivan Reitman"))
 
-// We can now use a switch statement with a mix of "is" and "as" operators to parse through the
-// things array and get information from it.
+// 我們現在可以使用混用了 "is" 運算子以及 "as" 運算子的 switch 表達式來取得這個陣列中元素的資訊
 //
-// We'll use a switch statement to check each type (and even include a few where clauses to get a
-// bit more specific about the value inside the type.) Note that when we do this, we use the forced
-// version of the 'as' operator, which is safe in the context of a 'case' because a matched case
-// means that it's guaranteed to be a valid value of the given type.
+// 我們將使用 switch 表達式來檢查每一個型別(甚至包含了這型別中更詳細的資訊)。注意，當我們這麼做的時候，我們使用強制轉
+// 換的 "as" 運算子，這在 case 區塊中是安全地操作，因為當 switch 中的特定 case 區塊被匹配成功時，就已經保證了這個
+// 傳入型別的合法性
 for thing in things
 {
 	switch thing
